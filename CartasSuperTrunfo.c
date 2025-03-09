f1710f9bd2bb0a1729d9d4b8898e037190594448
FernandaMagno <fernandadyjesus@gmail.com>
2025-03-09 15:51:37 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,43 @@
 #include <stdio.h>
-
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+#include <string.h>
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    float area, pib, densidadePopu; 
+    int populacao, pontoTu, codigoCidade;
+    char nome[50];
     
     // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
+    printf("Informe o nome da cidade: \n");
+    fgets(nome, sizeof(nome), stdin);
+    nome[strcspn(nome, "\n")] = '\0';
+
+    printf("Informe o codigo da cidade: \n");
+    scanf("%d", &codigoCidade);
+
+    printf("Informe o numero de pontos turisticos: \n");
+    scanf("%d", &pontoTu);
+
+    printf("Informe o numero de habitantes: \n");
+    scanf("%d", &populacao);
+
+    printf("Informe o PIB(Produto Interno Bruto): \n");
+    scanf("%f", &pib);
+
+    printf("Informe a area da cidade: \n");
+    scanf("%f", &area);
+
+    densidadePopu = populacao / area;
+
     // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("\n-------- Visao Geral da Cidade--------\n");
+    printf("Nome da cidade: %s\n", nome);
+    printf("Codigo da Cidade: %d\n", codigoCidade);
+    printf("Pontos Turisticos: %d\n", pontoTu);
+    printf("Habitantes: %d\n", populacao);
+    printf("PIB: %.2f\n", pib);
+    printf("Area em km2: %.2f\n", area);
+    printf("Densidade populacional: %.2f habitantes por km quadrado.\n", densidadePopu);
+
 
     return 0;
 }
